a74040dc5b92bb9b4b0ebd48b97e94c728221a65
michal sagi <michaldinner@gmail.com>
2022-08-03 13:34:19 +0300

    add playlist

--- BL/playlistLogic.js
+++ BL/playlistLogic.js
@@ -2,100 +2,110 @@ const userController = require("../DL/controllers/userController");
 const playlistController = require("../DL/controllers/playlistController");
 
 const playlist = async (id) => {
-  const playlist = await playlistController.readOne({ _id: id });
-  return playlist;
+    const playlist = await playlistController.readOne({ _id: id });
+    return playlist;
 };
 
 const getPlaylist = async (id) => {
-  const playlist = await playlistController.readOne({ _id: id });
-  const songs = await playlist.songs.map((song) => {
-    return {
-      id: song.songId,
-      title: song.songTitle,
-      img: song.img,
-    };
-  });
-  return songs;
+    const playlist = await playlistController.readOne({ _id: id });
+    const songs = await playlist.songs.map((song) => {
+        return {
+            id: song.songId,
+            title: song.songTitle,
+            img: song.img,
+        };
+    });
+    return songs;
 };
 
 const songList = async (playlist) => {
-  const songs = await playlistController.read({ _id: playlist });
-  return songs;
+    const songs = await playlistController.read({ _id: playlist });
+    return songs;
 };
 
 const addPlaylist = async (playlist) => {
-  console.log(playlist);
-  if (!playlist.name) {
-    throw { code: 400, message: "Error - name" };
-  }
-  if (!playlist.userId) {
-    throw { code: 400, message: "Error - user" };
-  }
+    console.log(playlist);
+    if (!playlist.name) {
+        throw { code: 400, message: "Error - name" };
+    }
+    if (!playlist.userId) {
+        throw { code: 400, message: "Error - user" };
+    }
 
-  const user = await userController.readOne({ _id: playlist.userId });
-  if (!user) {
-    throw { code: 400, message: "Error - user not found" };
-  }
+    const user = await userController.readOne({ _id: playlist.userId });
+    if (!user) {
+        throw { code: 400, message: "Error - user not found" };
+    }
 
-  return await playlistController.create(playlist);
+    if (playlist.songs) {
+        const newPlaylist = {
+            name: playlist.name,
+            userId: playlist.userId,
+            songs: [{ songId: playlist.songs, songTitle: playlist.songTitle, img: playlist.img }],
+        };
+        return await playlistController.create(newPlaylist);
+
+    } else {
+        return await playlistController.create(playlist);
+    }
+
 };
 
 const addSong = async ({ songId, playlistId, songTitle, img }) => {
-  if (!songId) {
-    ({ code: 400, message: "Error -song not found" });
-  }
-  if (!playlistId) {
-    ({ code: 400, message: "Error -playlist not found" });
-  }
-  const playlist = await playlistController.readOne({ _id: playlistId });
-  if (!playlist) {
-    ({ code: 400, message: "Error -playlist not found" });
-  }
-  for (i of playlist.songs) {
-    if (i === songId) {
-      throw { code: 400, message: "song exist in playlist" };
+    if (!songId) {
+        ({ code: 400, message: "Error -song not found" });
     }
-  }
-  return await playlistController.update(
-    { _id: playlistId },
-    { songs: [...playlist.songs, { songId, songTitle, img }] }
-  );
+    if (!playlistId) {
+        ({ code: 400, message: "Error -playlist not found" });
+    }
+    const playlist = await playlistController.readOne({ _id: playlistId });
+    if (!playlist) {
+        ({ code: 400, message: "Error -playlist not found" });
+    }
+    for (i of playlist.songs) {
+        if (i === songId) {
+            throw { code: 400, message: "song exist in playlist" };
+        }
+    }
+    return await playlistController.update(
+        { _id: playlistId },
+        { songs: [...playlist.songs, { songId, songTitle, img }] }
+    );
 };
 const allPlaylists = async (userId) => {
-  const user = await userController.readOne({ _id: userId });
-  if (!user) throw { code: 400, message: "Error - user not found" };
-  const playlists = await playlistController.read(
-    { userId, isActive: true },
-    "songs name"
-  );
-  return playlists;
+    const user = await userController.readOne({ _id: userId });
+    if (!user) throw { code: 400, message: "Error - user not found" };
+    const playlists = await playlistController.read(
+        { userId, isActive: true },
+        "songs name"
+    );
+    return playlists;
 };
 
-const delplaylist = async (playlistId) => {
-  const playlist = await playlistController.deletePlaylist({ _id: playlistId });
-  if (playlist.length === 0)
-    throw { code: 400, message: "no playlist in this id" };
+const delPlaylist = async (playlistId) => {
+    const playlist = await playlistController.deletePlaylist({ _id: playlistId });
+    if (playlist.length === 0)
+        throw { code: 400, message: "no playlist in this id" };
 
-  return playlist;
+    return playlist;
 };
 
 const delSong = async (idPlaylist, idSong) => {
-  const playlist = await playlistController.readOne({ _id: idPlaylist });
-  const index = playlist.songs.indexOf(idSong);
-  playlist.songs.splice(index, 1);
-  return await playlistController.update(
-    { _id: idPlaylist },
-    { songs: playlist.songs }
-  );
+    const playlist = await playlistController.readOne({ _id: idPlaylist });
+    const index = playlist.songs.indexOf(idSong);
+    playlist.songs.splice(index, 1);
+    return await playlistController.update(
+        { _id: idPlaylist },
+        { songs: playlist.songs }
+    );
 };
 module.exports = {
-  allPlaylists,
-  getPlaylist,
-  addPlaylist,
-  addSong,
-  firstPlaylist,
-  songList,
-  delPlaylist,
-  playlist,
-  delSong,
+    allPlaylists,
+    getPlaylist,
+    addPlaylist,
+    addSong,
+    songList,
+    delPlaylist,
+    playlist,
+    delSong,
 };
